8298c08ba8316b20b68177f633bee350ced51618
Alpha90 <94451773+m4k4r4n7h@users.noreply.github.com>
2025-01-19 16:55:18 +0530

    Add files via upload

---
+++ ctf.sh
@@ -0,0 +1,74 @@
+echo "Solve this Intermediate ctf..." | lolcat
+sleep 1
+bash ./.gitcomponents/controller.sh 2> /dev/null
+clear
+echo 5 | figlet | lolcat
+sleep 1
+clear
+echo 4 | figlet | lolcat
+sleep 1
+clear
+echo 3 | figlet | lolcat
+sleep 1
+clear
+echo 2 | figlet | lolcat
+sleep 1
+clear
+echo 1 | figlet | lolcat
+sleep 1
+clear
+
+echo ""
+sleep 2
+clear
+echo "Find the controller file in your current directory and make a backup." | lolcat
+sleep 2
+echo "The file named controller.sh will be removed in " | lolcat
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 15
+echo "The file named contoller.sh is deleted" | lolcat
+sleep 1
+clear
+rm controller.sh 2> /dev/null
+echo "Study the file named contoller to continue." | lolcat
+sleep 10
+
+
+
+
+
+
+sleep 1
+echo " "
+echo "Now its" | figlet | lolcat
+sleep 1
+echo " "
+clear
+echo "Time" | figlet | lolcat
+sleep 0.5
+echo "To" | figlet | lolcat
+sleep 0.5
+echo "Enter" | figlet | lolcat
+sleep 0.5
+echo "The" | figlet | lolcat
+sleep 0.5
+echo "Flag" | figlet | lolcat
+sleep 1.5
+clear
+
+
+
+
+python3 -c "exec(compile(chr(105)+chr(109)+chr(112)+chr(111)+chr(114)+chr(116)+chr(32)+chr(116)+chr(105)+chr(109)+chr(101)+chr(10)+chr(105)+chr(109)+chr(112)+chr(111)+chr(114)+chr(116)+chr(32)+chr(114)+chr(97)+chr(110)+chr(100)+chr(111)+chr(109)+chr(10)+chr(100)+chr(101)+chr(102)+chr(32)+chr(100)+chr(101)+chr(99)+chr(111)+chr(100)+chr(101)+chr(40)+chr(115)+chr(121)+chr(109)+chr(98)+chr(111)+chr(108)+chr(115)+chr(41)+chr(58)+chr(10)+chr(32)+chr(32)+chr(32)+chr(32)+chr(114)+chr(101)+chr(116)+chr(117)+chr(114)+chr(110)+chr(32)+chr(39)+chr(39)+chr(46)+chr(106)+chr(111)+chr(105)+chr(110)+chr(40)+chr(99)+chr(104)+chr(114)+chr(40)+chr(111)+chr(114)+chr(100)+chr(40)+chr(99)+chr(104)+chr(97)+chr(114)+chr(41)+chr(32)+chr(45)+chr(32)+chr(49)+chr(48)+chr(48)+chr(48)+chr(41)+chr(32)+chr(102)+chr(111)+chr(114)+chr(32)+chr(99)+chr(104)+chr(97)+chr(114)+chr(32)+chr(105)+chr(110)+chr(32)+chr(115)+chr(121)+chr(109)+chr(98)+chr(111)+chr(108)+chr(115)+chr(41)+chr(10)+chr(102)+chr(108)+chr(97)+chr(103)+chr(32)+chr(61)+chr(32)+chr(105)+chr(110)+chr(112)+chr(117)+chr(116)+chr(40)+chr(34)+chr(69)+chr(110)+chr(116)+chr(101)+chr(114)+chr(32)+chr(116)+chr(104)+chr(101)+chr(32)+chr(102)+chr(108)+chr(97)+chr(103)+chr(58)+chr(32)+chr(34)+chr(41)+chr(10)+chr(115)+chr(116)+chr(114)+chr(105)+chr(110)+chr(103)+chr(32)+chr(61)+chr(32)+chr(34)+chr(1067)+chr(1121)+chr(1066)+chr(1101)+chr(1082)+chr(1102)+chr(1079)+chr(1114)+chr(1071)+chr(1101)+chr(1082)+chr(1115)+chr(1123)+chr(1084)+chr(1104)+chr(1049)+chr(1053)+chr(1095)+chr(1119)+chr(1052)+chr(1053)+chr(1095)+chr(1083)+chr(1048)+chr(1109)+chr(1051)+chr(1095)+chr(1069)+chr(1052)+chr(1097)+chr(1115)+chr(1121)+chr(1095)+chr(1083)+chr(1116)+chr(1117)+chr(1102)+chr(1102)+chr(1125)+chr(34)+chr(46)+chr(115)+chr(116)+chr(114)+chr(105)+chr(112)+chr(40)+chr(41)+chr(10)+chr(116)+chr(101)+chr(120)+chr(116)+chr(32)+chr(61)+chr(32)+chr(100)+chr(101)+chr(99)+chr(111)+chr(100)+chr(101)+chr(40)+chr(115)+chr(116)+chr(114)+chr(105)+chr(110)+chr(103)+chr(41)+chr(10)+chr(105)+chr(102)+chr(32)+chr(102)+chr(108)+chr(97)+chr(103)+chr(32)+chr(61)+chr(61)+chr(32)+chr(116)+chr(101)+chr(120)+chr(116)+chr(58)+chr(10)+chr(32)+chr(32)+chr(32)+chr(32)+chr(112)+chr(114)+chr(105)+chr(110)+chr(116)+chr(40)+chr(34)+chr(67)+chr(111)+chr(110)+chr(103)+chr(114)+chr(97)+chr(116)+chr(115)+chr(46)+chr(46)+chr(44)+chr(32)+chr(89)+chr(111)+chr(117)+chr(32)+chr(99)+chr(97)+chr(110)+chr(32)+chr(110)+chr(111)+chr(119)+chr(32)+chr(98)+chr(101)+chr(32)+chr(109)+chr(101)+chr(109)+chr(98)+chr(101)+chr(114)+chr(32)+chr(111)+chr(102)+chr(32)+chr(111)+chr(117)+chr(114)+chr(32)+chr(116)+chr(101)+chr(97)+chr(109)+chr(46)+chr(32)+chr(74)+chr(111)+chr(105)+chr(110)+chr(58)+chr(32)+chr(104)+chr(116)+chr(116)+chr(112)+chr(115)+chr(58)+chr(47)+chr(47)+chr(100)+chr(105)+chr(115)+chr(99)+chr(111)+chr(114)+chr(100)+chr(46)+chr(103)+chr(103)+chr(47)+chr(66)+chr(52)+chr(90)+chr(88)+chr(82)+chr(112)+chr(81)+chr(87)+chr(32)+chr(34)+chr(41)+chr(10)+chr(101)+chr(108)+chr(115)+chr(101)+chr(58)+chr(10)+chr(32)+chr(32)+chr(32)+chr(32)+chr(112)+chr(114)+chr(105)+chr(110)+chr(116)+chr(40)+chr(34)+chr(84)+chr(114)+chr(121)+chr(32)+chr(97)+chr(103)+chr(97)+chr(105)+chr(110)+chr(46)+chr(46)+chr(46)+chr(34)+chr(41)+chr(10)+chr(10), chr(60)+chr(115)+chr(99)+chr(114)+chr(105)+chr(112)+chr(116)+chr(62), chr(101)+chr(120)+chr(101)+chr(99)))" | lolcat
+
+
+
+
